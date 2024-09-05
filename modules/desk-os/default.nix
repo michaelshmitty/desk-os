1c0091fb658fa201fb0269c1306d2e3422a0f11f
Michael Smith <m@michaelsmith.be>
2024-09-05 22:59:03 +0200

    Make boot more quiet

--- modules/desk-os/default.nix
+++ modules/desk-os/default.nix
@@ -9,7 +9,7 @@
 
   boot = {
     consoleLogLevel = 0;
-    kernelParams = ["quiet"];
+    kernelParams = ["quiet" "splash" "systemd.show_status=auto" "rd.udev.log_level=3"];
     initrd.verbose = false;
     loader.systemd-boot.enable = true;
     loader.systemd-boot.configurationLimit = 3;
