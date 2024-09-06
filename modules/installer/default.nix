4e335647a4818b37f2db11e9293cf9c360a030ec
Michael Smith <m@michaelsmith.be>
2024-09-06 10:04:30 +0200

    Update distroName for installer

--- modules/installer/default.nix
+++ modules/installer/default.nix
@@ -21,6 +21,8 @@ in {
     (modulesPath + "/profiles/installation-device.nix")
   ];
 
+  system.nixos.distroName = "deskOS";
+
   boot = {
     consoleLogLevel = 0;
     kernelParams = ["quiet" "splash" "systemd.show_status=auto" "rd.udev.log_level=3"];
